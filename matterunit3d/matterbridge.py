9dfd28a9e468497382e8759bb41b1624dc1f90fc
root <aithercc_heaven@protonmail.com>
2024-11-21 14:08:27 +0000

    Cleanup unused code

--- matterunit3d/matterbridge.py
+++ matterunit3d/matterbridge.py
@@ -11,21 +11,6 @@ class matterbridge(service):
 
         return self.app.session.request(method, url, *args, headers = headers, **kwargs)
 
-    def post(self, url, payload, headers):
-        url = self.addr + url
-
-        return self.app.session.post(url, json = payload, headers = headers)
-
-    async def send(self, message_id, username, msg, created_at):
-        headers = {'Content-Type': 'application/json'}
-        payload = {
-            'username': username,
-            'text': msg,
-            'gateway': self.gateway
-        }
-
-        return await self.post("/api/message", payload, headers)
-
     # Read message from Matterbridge API stream and pass them to unit3d.send() (IRC -> Chatbox)
     async def watch(self):
         async with self.request("GET", "/api/stream") as req:
